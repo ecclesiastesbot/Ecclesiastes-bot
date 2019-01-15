8b2246934964f4fec7ee32e2252bedc9e9aa7419
ecclesiastesbot <46635778+ecclesiastesbot@users.noreply.github.com>
2019-01-15 05:10:52 +0200

    Add files via upload
    Finalized Changes, no further things needed for further functionality

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # bot.py
-
+from os import environ
 from generate_verses import get_verselist
 import tweepy
 import json
 import time
-from secrets import *
+
+CONSUMER_KEY = environ["CONSUMER_KEY"]
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 INTERVAL = 60 * 60 * 24 # tweets every 24 hours
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 verselist = get_verselist()
@@ -19,5 +23,3 @@ my_iter = iter(verselist)
 while True:
     api.update_status(next(my_iter))
     time.sleep(INTERVAL)
-
-api.update_status("Beep")
